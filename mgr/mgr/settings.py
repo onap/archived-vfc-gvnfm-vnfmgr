67bc77dd3de414bcbf26a64eb0a777debc0f99df
ying.yunlong <ying.yunlong@zte.com.cn>
2018-03-22 17:18:55 +0800

    Add vfc-vnfmgr log integration config
    Change-Id: I492453c20de02319cd28f601baf924dc9bfa2e42
    Issue-ID: VFC-839
    Signed-off-by: ying.yunlong <ying.yunlong@zte.com.cn>

--- mgr/mgr/settings.py
+++ mgr/mgr/settings.py
@@ -19,6 +19,9 @@ import redisco
 
 from mgr.pub.config.config import REDIS_HOST, REDIS_PORT, REDIS_PASSWD
 from mgr.pub.config.config import DB_NAME, DB_IP, DB_USER, DB_PASSWD, DB_PORT
+from logging import config
+from onaplogging import monkey
+monkey.patch_all()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -57,6 +60,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mgr.middleware.LogContextMiddleware',
 ]
 
 ROOT_URLCONF = 'mgr.urls'
@@ -118,36 +122,42 @@ TIME_ZONE = 'UTC'
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+#
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': True,
+#     'formatters': {
+#         'standard': {
+#             'format': '%(asctime)s:[%(name)s]:[%(filename)s]-[%(lineno)d] [%(levelname)s]:%(message)s',
+#         },
+#     },
+#     'filters': {
+#     },
+#     'handlers': {
+#         'mgr_handler': {
+#             'level': 'DEBUG',
+#             'class': 'logging.handlers.RotatingFileHandler',
+#             'filename': os.path.join(BASE_DIR, 'logs/runtime_mgr.log'),
+#             'formatter': 'standard',
+#             'maxBytes': 1024 * 1024 * 50,
+#             'backupCount': 5,
+#         },
+#     },
+#
+#     'loggers': {
+#         'mgr': {
+#             'handlers': ['mgr_handler'],
+#             'level': 'DEBUG',
+#             'propagate': False
+#         },
+#     }
+# }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s:[%(name)s]:[%(filename)s]-[%(lineno)d] [%(levelname)s]:%(message)s',
-        },
-    },
-    'filters': {
-    },
-    'handlers': {
-        'mgr_handler': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/runtime_mgr.log'),
-            'formatter': 'standard',
-            'maxBytes': 1024 * 1024 * 50,
-            'backupCount': 5,
-        },
-    },
+LOGGING_CONFIG = None
+# yaml configuration of logging
+LOGGING_FILE = os.path.join(BASE_DIR, 'mgr/log.yml')
+config.yamlConfig(filepath=LOGGING_FILE, watchDog=True)
 
-    'loggers': {
-        'mgr': {
-            'handlers': ['mgr_handler'],
-            'level': 'DEBUG',
-            'propagate': False
-        },
-    }
-}
 
 if 'test' in sys.argv:
     from mgr.pub.config import config
