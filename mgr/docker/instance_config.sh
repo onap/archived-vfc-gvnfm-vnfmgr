f6a5ff84bb5d42e2ee8d6952297aea340502d8d1
yangyan <yangyanyj@chinamobile.com>
2019-03-26 10:20:01 +0800

    update redis service name
    Change-Id: I1568a446c518b87c06517fdf465251c1a309b5dc
    Issue-ID: VFC-1316
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- mgr/docker/instance_config.sh
+++ mgr/docker/instance_config.sh
@@ -20,8 +20,18 @@ MYSQL_IP=`echo $MYSQL_ADDR | cut -d: -f 1`
 MYSQL_PORT=`echo $MYSQL_ADDR | cut -d: -f 2`
 echo "MYSQL_ADDR=$MYSQL_ADDR"
 
+MYSQL_USER=`echo $MYSQL_AUTH | cut -d: -f 1`
+MYSQL_ROOT_PASSWORD=`echo $MYSQL_AUTH | cut -d: -f 2`
+
+REDIS_IP=`echo $REDIS_ADDR | cut -d: -f 1`
+REDIS_PORT=`echo $REDIS_ADDR | cut -d: -f 2`
+
 sed -i "s|DB_IP.*|DB_IP = '$MYSQL_IP'|" vfc/gvnfm/vnfmgr/mgr/mgr/pub/config/config.py
 sed -i "s|DB_PORT.*|DB_PORT = $MYSQL_PORT|" vfc/gvnfm/vnfmgr/mgr/mgr/pub/config/config.py
-sed -i "s|REDIS_HOST.*|REDIS_HOST = '$MYSQL_IP'|" vfc/gvnfm/vnfmgr/mgr/mgr/pub/config/config.py
+sed -i "s|REDIS_HOST.*|REDIS_HOST = '$REDIS_IP'|" vfc/gvnfm/vnfmgr/mgr/mgr/pub/config/config.py
+sed -i "s|REDIS_PORT.*|REDIS_PORT = '$REDIS_PORT'|" vfc/gvnfm/vnfmgr/mgr/mgr/pub/config/config.py
+sed -i "s|DB_USER.*|DB_USER = '$MYSQL_USER'|" vfc/gvnfm/vnfmgr/mgr/mgr/pub/config/config.py
+sed -i "s|DB_PASSWD.*|DB_PASSWD = '$MYSQL_ROOT_PASSWORD'|" vfc/gvnfm/vnfmgr/mgr/mgr/pub/config/config.py
+
 
 cat vfc/gvnfm/vnfmgr/mgr/mgr/pub/config/config.py
