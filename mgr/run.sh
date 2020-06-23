007b688fd4064f65e09bfac2f2ec941a642875c2
dengyh <dengyuanhong@chinamobile.com>
2020-06-23 16:59:57 +0800

    Implementation of HTTPS support for VFC-vnfmgr project interface
    Change-Id: I7ee9e2691e2584aa196fa158d7b15c7fd841c577
    Issue-ID: VFC-1676
    Signed-off-by: dengyh <dengyuanhong@chinamobile.com>

--- mgr/run.sh
+++ mgr/run.sh
@@ -18,10 +18,10 @@ if [ ! -x  $logDir  ]; then
        mkdir -p $logDir
 fi
 
-nohup python manage.py runserver 0.0.0.0:8803 > /dev/null &
+# nohup python manage.py runserver 0.0.0.0:8803 > /dev/null &
 
-# if [ "${SSL_ENABLED}" = "true" ]; then
-#     nohup uwsgi --https :8803,mgr/pub/ssl/cert/foobar.crt,mgr/pub/ssl/cert/foobar.key, -t 120 --enable-threads --module mgr.wsgi --master --processes 4 &
-# else
-#     nohup uwsgi --http :8803 -t 120 --enable-threads --module mgr.wsgi --master --processes 4 &
-# fi
+if [ "${SSL_ENABLED}" = "true" ]; then
+   nohup uwsgi --https :8803,mgr/pub/ssl/cert/foobar.crt,mgr/pub/ssl/cert/foobar.key, -t 120 --enable-threads --module mgr.wsgi --master --processes 4 &
+else
+   nohup uwsgi --http :8803 -t 120 --enable-threads --module mgr.wsgi --master --processes 4 &
+fi
